ae21c16bf239c4da61b966f81190eb797692848e
Harry <harry@wolfhound.ie>
2015-03-17 10:41:02 +0000

    Some updates to Main Demo

--- build-sass.sh
+++ build-sass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sass -t compressed sass/ireland-glyph-font.scss:css/ireland-glyph-font.min.css
-sass -t expanded sass/ireland-glyph-font.scss:css/ireland-glyph-font.max.css
-sass -t compressed sass/style.scss:css/style.min.css
-sass -t expanded sass/style.scss:css/style.css
+sass -t compressed sass/ireland-glyph-font.scss:css/ireland-glyph-font.min.css --sourcemap=none
+sass -t expanded sass/ireland-glyph-font.scss:css/ireland-glyph-font.max.css --sourcemap=none
+sass -t compressed sass/style.scss:css/style.min.css --sourcemap=none
+sass -t expanded sass/style.scss:css/style.css --sourcemap=none
